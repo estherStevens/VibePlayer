b041754974adc6917035b13f3c384acdad59d407
estherstevens <esther.stevens7@yahoo.co.uk>
2025-12-15 17:20:04 +0000

    implement tracks screen

--- app/src/main/java/com/stevens/software/vibeplayer/PermissionScreen.kt
+++ app/src/main/java/com/stevens/software/vibeplayer/PermissionScreen.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,12 +13,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -78,6 +81,12 @@ internal fun PermissionView(
                 .padding(horizontal = 18.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier.size(56.dp)
+            )
+            Spacer(Modifier.size(20.dp))
             Text(
                 text = stringResource(R.string.app_name),
                 color = MaterialTheme.extendedColours.textPrimary,
--- app/src/main/java/com/stevens/software/vibeplayer/VibePlayer.kt
+++ app/src/main/java/com/stevens/software/vibeplayer/VibePlayer.kt
@@ -4,14 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -66,8 +69,55 @@ private fun EmptyState(){
     }
 }
 
+@Composable
+private fun TracksState(){
+    TrackItem()
+}
+
+
+@Composable
+private fun TrackItem(){
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(64.dp)
+                .clip(RoundedCornerShape(10.dp))
+        ) {
+
+        }
+        Spacer(Modifier.size(12.dp))
+        Column {
+            Text(
+                text = "505",
+                color = MaterialTheme.extendedColours.textPrimary,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(Modifier.size(2.dp))
+            Text(
+                text = "Arctic Monkeys",
+                color = MaterialTheme.extendedColours.textSecondary,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Spacer(Modifier.weight(1f))
+        Text(
+            text = "4:14",
+            color = MaterialTheme.extendedColours.textSecondary,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
 @Preview(showSystemUi = true)
 @Composable
 private fun EmptyStateView(){
     EmptyState()
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun TracksView(){
+    TracksState()
 }
