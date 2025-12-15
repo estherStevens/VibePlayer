315660ea3858eb1683a4785b4c3c8b4c15bc1720
estherstevens <esther.stevens7@yahoo.co.uk>
2025-12-15 17:53:08 +0000

    implement scanner screen

--- app/src/main/java/com/stevens/software/vibeplayer/VibePlayer.kt
+++ app/src/main/java/com/stevens/software/vibeplayer/VibePlayer.kt
@@ -1,5 +1,12 @@
 package com.stevens.software.vibeplayer
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,14 +14,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,6 +43,7 @@ fun VibePlayerScreen(viewModel: VibePlayerViewModel) {
     VibePlayerView(uiState.value)
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun VibePlayerView(uiState: VibePlayerState) {
     Box(
@@ -35,11 +51,34 @@ fun VibePlayerView(uiState: VibePlayerState) {
             .fillMaxSize()
             .background(MaterialTheme.extendedColours.bg)
     ) {
-        when(uiState) {
-            VibePlayerState.Empty -> EmptyState()
-            VibePlayerState.Scanning -> {}
-            VibePlayerState.Tracks -> {}
+        Column {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        color = MaterialTheme.extendedColours.accent,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                },
+                navigationIcon = {
+                    Image(
+                        painter = painterResource(R.drawable.logo_small),
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 4.dp)
+                    )
+                },
+                modifier = Modifier.padding(horizontal = 16.dp),
+                colors = TopAppBarDefaults.topAppBarColors().copy(
+                    containerColor = MaterialTheme.extendedColours.bg
+                )
+            )
+            when(uiState) {
+                VibePlayerState.Empty -> EmptyState()
+                VibePlayerState.Scanning -> ScannerState()
+                VibePlayerState.Tracks -> TracksState()
+            }
         }
+
     }
 }
 
@@ -70,10 +109,50 @@ private fun EmptyState(){
 }
 
 @Composable
+private fun ScannerState(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Scanner()
+        Spacer(Modifier.size(20.dp))
+        Text(
+            text = stringResource(R.string.scanner_scanning_subtitle),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.extendedColours.textSecondary
+        )
+    }
+
+}
+
+@Composable
+private fun Scanner(){
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite")
+
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 2000,
+                easing = LinearEasing
+            ),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "rotation"
+    )
+    Image(
+        painter = painterResource(R.drawable.scanner),
+        contentDescription = stringResource(R.string.scanner_scanning),
+        modifier = Modifier.rotate(rotation)
+    )
+}
+
+@Composable
 private fun TracksState(){
     TrackItem()
 }
-
 
 @Composable
 private fun TrackItem(){
@@ -113,11 +192,17 @@ private fun TrackItem(){
 @Preview(showSystemUi = true)
 @Composable
 private fun EmptyStateView(){
-    EmptyState()
+    VibePlayerView(VibePlayerState.Empty)
 }
 
 @Preview(showSystemUi = true)
 @Composable
 private fun TracksView(){
-    TracksState()
+    VibePlayerView(VibePlayerState.Tracks)
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun ScannerView(){
+    VibePlayerView(VibePlayerState.Scanning)
 }
