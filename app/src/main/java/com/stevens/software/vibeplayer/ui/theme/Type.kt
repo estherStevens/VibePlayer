0481c70d3138648ef7b1f6525f99481407d6dfab
estherstevens <esther.stevens7@yahoo.co.uk>
2025-12-15 15:06:00 +0000

    add typograpthy

--- app/src/main/java/com/stevens/software/vibeplayer/ui/theme/Type.kt
+++ app/src/main/java/com/stevens/software/vibeplayer/ui/theme/Type.kt
@@ -2,33 +2,42 @@ package com.stevens.software.vibeplayer.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.stevens.software.vibeplayer.R
 
-// Set of Material typography styles to start with
+val fontFamily = FontFamily(
+    Font(R.font.hostgrotesk_medium, FontWeight.Medium),
+    Font(R.font.hostgrotesk_semibold, FontWeight.SemiBold),
+    Font(R.font.hostgrotesk_regular, FontWeight.Normal)
+)
+
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp,
+        lineHeight = 32.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 22.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    bodyMedium = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 28.sp
     )
-    */
 )
+
